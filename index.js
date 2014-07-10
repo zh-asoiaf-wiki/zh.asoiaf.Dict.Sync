c0ef6cb99726849dde716cf2f971b6c42f846d3d
chenchenyangll <ccy-8888@hotmail.com>
2014-07-11 01:38:43 +0800

    sync

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 var express = require("express");
-var dict = require("zh.asoiaf.dict");
+var Dict = require("zh.asoiaf.dict");
+var dict = new Dict({
+  config: 'dict_config.js'
+});
 
 var app = express();
 app.get('/dict', function(req, res) {
-  console.log('in /dict');
+  console.log('/dict');
   res.download('./dict-all.json', 'dict.json', function(err) {
     if (err) {
       console.log(err);
     } else {
-      console.log('get');
+      setTimeout(sync, 10000); // when dict is downloaded, sync dict 15 min later
     }
   });
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(80, function() {
   console.log('Server start...');
+  
+  setInterval(sync, 30000); // sync dict every 1 hour
 });
+
+var sync = function() {
+  dict.getAll();
+  console.log('dict sync.');
+};
