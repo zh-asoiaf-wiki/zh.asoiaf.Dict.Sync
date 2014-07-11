44ec9d8b7a276c1ca249a74e958758c2da57761d
ccy <ccy-8888@hotmail.com>
2014-07-11 09:10:28 +0800

    use env var to pass username & password & port

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var express = require("express");
 var Dict = require("zh.asoiaf.dict");
 var dict = new Dict({
-  config: 'dict_config.js'
+  config: {
+    "server": "zh.asoiaf.wikia.com", 
+    "path": "", 
+    "username": process.env.BOT_USERNAME, 
+    "password": process.env.BOT_PASSWORD, 
+    "userAgent": "zh.asoiaf.Dict", 
+    "debug": true  
+  }
 });
 
 var app = express();
@@ -11,15 +18,16 @@ app.get('/dict', function(req, res) {
     if (err) {
       console.log(err);
     } else {
-      setTimeout(sync, 10000); // when dict is downloaded, sync dict 15 min later
+      setTimeout(sync, 900000); // when dict is downloaded, sync dict 15 min later
     }
   });
 });
 
-var server = app.listen(80, function() {
+var port = process.env.PORT || 7777;
+var server = app.listen(port, function() {
   console.log('Server start...');
   
-  setInterval(sync, 30000); // sync dict every 1 hour
+  setInterval(sync, 3600000); // sync dict every 1 hour
 });
 
 var sync = function() {
